b1664a0a687a0dab836f38b8cc5bff449d6e1bcb
Jotiba Chopade <jotiba.chopade@expleogroup.com>
2021-08-11 17:20:37 +0530

    updated

--- src/main/java/com/qa/democart/pages/ProfilePage.java
+++ src/main/java/com/qa/democart/pages/ProfilePage.java
@@ -1,6 +1,10 @@
 package com.qa.democart.pages;
 
+import org.openqa.selenium.By;
+
 public class ProfilePage {
+	
+	private By profile= By.id("profile");
 	
 	public ProfilePage() {
 		System.out.println("PP--default const...");
@@ -8,6 +12,7 @@ public class ProfilePage {
 	
 	public void getTitle() {
 		System.out.println("PP---title");
+		System.out.println("Click on profile");
 	}
 
 }
