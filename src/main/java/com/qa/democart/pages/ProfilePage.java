35c4a2ee16dbc1420ed0c2ab62c5ba0a7c0f14a6
Jotiba Chopade <jotiba.chopade@expleogroup.com>
2021-08-11 16:36:26 +0530

    created profile page java class

---
+++ src/main/java/com/qa/democart/pages/ProfilePage.java
@@ -0,0 +1,13 @@
+package com.qa.democart.pages;
+
+public class ProfilePage {
+	
+	public ProfilePage() {
+		System.out.println("PP--default const...");
+	}
+	
+	public void getTitle() {
+		System.out.println("PP---title");
+	}
+
+}
